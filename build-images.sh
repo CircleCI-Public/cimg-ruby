493e6fb623c8c8cfb216d24c61b4b6eac2977b2c
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-03-23 22:57:37 -0400

    Respin 2.7.0 (#29)
    * Respin Ruby v2.7.0 to enable node variant. [release]
    
    * Add the Node.js variant to readme.

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 2.6/Dockerfile -t cimg/ruby:2.6.5  -t cimg/ruby:2.6 .
-docker build --file 2.6/node/Dockerfile -t cimg/ruby:2.6.5-node  -t cimg/ruby:2.6-node .
+docker build --file 2.7/Dockerfile -t cimg/ruby:2.7.0  -t cimg/ruby:2.7 .
+docker build --file 2.7/node/Dockerfile -t cimg/ruby:2.7.0-node  -t cimg/ruby:2.7-node .
