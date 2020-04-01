a086c6d0c5497930596515e7930ceed846dac268
Josh Frye <josh@joshfrye.com>
2020-04-01 07:19:26 -0400

    Ruby 2.7.1
    https://www.ruby-lang.org/en/news/2020/03/31/ruby-2-7-1-released/

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 2.7/Dockerfile -t cimg/ruby:2.7.0  -t cimg/ruby:2.7 .
-docker build --file 2.7/node/Dockerfile -t cimg/ruby:2.7.0-node  -t cimg/ruby:2.7-node .
+docker build --file 2.7/Dockerfile -t cimg/ruby:2.7.1  -t cimg/ruby:2.7 .
+docker build --file 2.7/node/Dockerfile -t cimg/ruby:2.7.1-node  -t cimg/ruby:2.7-node .
