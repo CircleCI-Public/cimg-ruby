a18ab80240e6789385200a1e6a3d9fdcd384df57
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-05-11 10:59:49 -0400

    Respin latest release for last 3 series. (#41)
    Respining to add important DB libraries before this image goes GA. Not
    including the v2.4.x series due to it becoming EOL days after these
    releases.

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+docker build --file 2.5/Dockerfile -t cimg/ruby:2.5.8  -t cimg/ruby:2.5 .
+docker build --file 2.5/node/Dockerfile -t cimg/ruby:2.5.8-node  -t cimg/ruby:2.5-node .
+docker build --file 2.6/Dockerfile -t cimg/ruby:2.6.6  -t cimg/ruby:2.6 .
+docker build --file 2.6/node/Dockerfile -t cimg/ruby:2.6.6-node  -t cimg/ruby:2.6-node .
 docker build --file 2.7/Dockerfile -t cimg/ruby:2.7.1  -t cimg/ruby:2.7 .
 docker build --file 2.7/node/Dockerfile -t cimg/ruby:2.7.1-node  -t cimg/ruby:2.7-node .
