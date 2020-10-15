107a7b5d5f1c7a79bcfc16673efca9d97a967e54
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-10-14 23:34:20 -0400

    Browser variant (#52)
    * Update shared tools.
    
    * Add browser variant.

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 2.6/node/Dockerfile -t cimg/ruby:2.6.2-node .
-
-docker build --file 2.7/Dockerfile -t cimg/ruby:2.7.2  -t cimg/ruby:2.7 .
-docker build --file 2.7/node/Dockerfile -t cimg/ruby:2.7.2-node  -t cimg/ruby:2.7-node .
+docker build --file 2.5/browsers/Dockerfile -t cimg/ruby:2.5.8-browsers  -t cimg/ruby:2.5-browsers .
+docker build --file 2.6/browsers/Dockerfile -t cimg/ruby:2.6.6-browsers  -t cimg/ruby:2.6-browsers .
+docker build --file 2.7/browsers/Dockerfile -t cimg/ruby:2.7.2-browsers  -t cimg/ruby:2.7-browsers .
