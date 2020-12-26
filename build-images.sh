8c92dda7a323229eedf3a5b934dcaabba42b8287
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-12-25 19:23:35 -0500

    Publish v3.0.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 2.5/browsers/Dockerfile -t cimg/ruby:2.5.8-browsers  -t cimg/ruby:2.5-browsers .
-docker build --file 2.6/browsers/Dockerfile -t cimg/ruby:2.6.6-browsers  -t cimg/ruby:2.6-browsers .
-docker build --file 2.7/browsers/Dockerfile -t cimg/ruby:2.7.2-browsers  -t cimg/ruby:2.7-browsers .
+docker build --file 3.0/Dockerfile -t cimg/ruby:3.0.0  -t cimg/ruby:3.0 .
+docker build --file 3.0/node/Dockerfile -t cimg/ruby:3.0.0-node  -t cimg/ruby:3.0-node .
+docker build --file 3.0/browsers/Dockerfile -t cimg/ruby:3.0.0-browsers  -t cimg/ruby:3.0-browsers .
