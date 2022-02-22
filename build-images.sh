60ee4af8fe5c716bb5ec6351ea4fb302f00874d1
Yuya.Nishida <yuya@j96.org>
2022-02-23 04:34:56 +0900

    run: git cococo ./shared/gen-dockerfiles.sh 3.1.1

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.1/Dockerfile -t cimg/ruby:3.1.0  -t cimg/ruby:3.1 .
-docker build --file 3.1/node/Dockerfile -t cimg/ruby:3.1.0-node  -t cimg/ruby:3.1-node .
-docker build --file 3.1/browsers/Dockerfile -t cimg/ruby:3.1.0-browsers  -t cimg/ruby:3.1-browsers .
+docker build --file 3.1/Dockerfile -t cimg/ruby:3.1.1  -t cimg/ruby:3.1 .
+docker build --file 3.1/node/Dockerfile -t cimg/ruby:3.1.1-node  -t cimg/ruby:3.1-node .
+docker build --file 3.1/browsers/Dockerfile -t cimg/ruby:3.1.1-browsers  -t cimg/ruby:3.1-browsers .
