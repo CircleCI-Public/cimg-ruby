6ae607884cfbeb00320ff213a3cac0129a82745e
Yuya.Nishida <62389+nishidayuya@users.noreply.github.com>
2022-02-24 01:58:48 +0900

    Release Ruby v3.1.1 (#87) [release]

---
+++ push-images.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+
+docker push cimg/ruby:3.1.1
+docker push cimg/ruby:3.1
+docker push cimg/ruby:3.1.1-node
+docker push cimg/ruby:3.1-node
+docker push cimg/ruby:3.1.1-browsers
+docker push cimg/ruby:3.1-browsers
