36df283cce0873dba6a2e8b309a7a41da4c1b452
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-04-12 16:39:38 -0400

    Pub: v2.6.10, v2.7.6, v3.0.1, and more. [release] (#90)

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/ruby:3.1.1
+docker push cimg/ruby:2.6.10
+docker push cimg/ruby:2.6
+docker push cimg/ruby:2.6.10-node
+docker push cimg/ruby:2.6-node
+docker push cimg/ruby:2.6.10-browsers
+docker push cimg/ruby:2.6-browsers
+
+docker push cimg/ruby:2.7.6
+docker push cimg/ruby:2.7
+docker push cimg/ruby:2.7.6-node
+docker push cimg/ruby:2.7-node
+docker push cimg/ruby:2.7.6-browsers
+docker push cimg/ruby:2.7-browsers
+
+docker push cimg/ruby:3.0.1
+docker push cimg/ruby:3.0
+docker push cimg/ruby:3.0.1-node
+docker push cimg/ruby:3.0-node
+docker push cimg/ruby:3.0.1-browsers
+docker push cimg/ruby:3.0-browsers
+
+docker push cimg/ruby:3.1.2
 docker push cimg/ruby:3.1
-docker push cimg/ruby:3.1.1-node
+docker push cimg/ruby:3.1.2-node
 docker push cimg/ruby:3.1-node
-docker push cimg/ruby:3.1.1-browsers
+docker push cimg/ruby:3.1.2-browsers
 docker push cimg/ruby:3.1-browsers
