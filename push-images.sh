11da84346134c498430b9137d60e732a7bbf9685
Jeff Chen <jeff.chen@circleci.com>
2022-10-04 13:05:03 -0700

    updates dockerfiles and generates gen-check

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/ruby:3.0.4
 docker push cimg/ruby:3.0
-docker push cimg/ruby:3.0.4-node
+docker push cimg/ruby:3.0.4
 docker push cimg/ruby:3.0-node
-docker push cimg/ruby:3.0.4-browsers
+docker push cimg/ruby:3.0.4-node
 docker push cimg/ruby:3.0-browsers
+docker push cimg/ruby:3.0.4-browsers
