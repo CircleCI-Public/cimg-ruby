96547664ce48de5fa636c549758274900f12ce83
Jalexchen <jeff.chen@circleci.com>
2022-10-05 13:39:13 -0700

    updates dockerfiles and generates gen-check (#94)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.0/Dockerfile -t cimg/ruby:3.0.4  -t cimg/ruby:3.0 .
-docker build --file 3.0/node/Dockerfile -t cimg/ruby:3.0.4-node  -t cimg/ruby:3.0-node .
-docker build --file 3.0/browsers/Dockerfile -t cimg/ruby:3.0.4-browsers  -t cimg/ruby:3.0-browsers .
+docker build --file 3.0/Dockerfile -t cimg/ruby:3.0.4 -t cimg/ruby:3.0 .
+docker build --file 3.0/node/Dockerfile -t cimg/ruby:3.0.4-node -t cimg/ruby:3.0-node .
+docker build --file 3.0/browsers/Dockerfile -t cimg/ruby:3.0.4-browsers -t cimg/ruby:3.0-browsers .
