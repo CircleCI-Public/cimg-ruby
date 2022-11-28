2cb03f2a4f02107a7994973c56c99978d925d779
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-11-28 15:28:25 +0000

    Release v2.7.7 v3.0.5 and more (#98)
    * Publish v2.7.7, v3.0.5, and v3.1.3. [release]
    
    * Update cimg-shared submodule
    
    * Publish v2.7.7, v3.0.5, and v3.1.3. [release]
    
    Co-authored-by: Stuart Chinery <stuart.chinery@boxt.co.uk>

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.0/Dockerfile -t cimg/ruby:3.0.4 -t cimg/ruby:3.0 .
-docker build --file 3.0/node/Dockerfile -t cimg/ruby:3.0.4-node -t cimg/ruby:3.0-node .
-docker build --file 3.0/browsers/Dockerfile -t cimg/ruby:3.0.4-browsers -t cimg/ruby:3.0-browsers .
+docker build --file 2.7/Dockerfile -t cimg/ruby:2.7.7 -t cimg/ruby:2.7 .
+docker build --file 2.7/node/Dockerfile -t cimg/ruby:2.7.7-node -t cimg/ruby:2.7-node .
+docker build --file 2.7/browsers/Dockerfile -t cimg/ruby:2.7.7-browsers -t cimg/ruby:2.7-browsers .
+docker build --file 3.0/Dockerfile -t cimg/ruby:3.0.5 -t cimg/ruby:3.0 .
+docker build --file 3.0/node/Dockerfile -t cimg/ruby:3.0.5-node -t cimg/ruby:3.0-node .
+docker build --file 3.0/browsers/Dockerfile -t cimg/ruby:3.0.5-browsers -t cimg/ruby:3.0-browsers .
+docker build --file 3.1/Dockerfile -t cimg/ruby:3.1.3 -t cimg/ruby:3.1 .
+docker build --file 3.1/node/Dockerfile -t cimg/ruby:3.1.3-node -t cimg/ruby:3.1-node .
+docker build --file 3.1/browsers/Dockerfile -t cimg/ruby:3.1.3-browsers -t cimg/ruby:3.1-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+docker push cimg/ruby:2.7
+docker push cimg/ruby:2.7.7
+docker push cimg/ruby:2.7-node
+docker push cimg/ruby:2.7.7-node
+docker push cimg/ruby:2.7-browsers
+docker push cimg/ruby:2.7.7-browsers
 docker push cimg/ruby:3.0
-docker push cimg/ruby:3.0.4
+docker push cimg/ruby:3.0.5
 docker push cimg/ruby:3.0-node
-docker push cimg/ruby:3.0.4-node
+docker push cimg/ruby:3.0.5-node
 docker push cimg/ruby:3.0-browsers
-docker push cimg/ruby:3.0.4-browsers
+docker push cimg/ruby:3.0.5-browsers
+docker push cimg/ruby:3.1
+docker push cimg/ruby:3.1.3
+docker push cimg/ruby:3.1-node
+docker push cimg/ruby:3.1.3-node
+docker push cimg/ruby:3.1-browsers
+docker push cimg/ruby:3.1.3-browsers
