dcabe31d89ea376a2ed7691886c1ddf09332802e
mkusaka <hinoshita1992@gmail.com>
2022-12-29 05:04:09 +0900

    Publish v3.2.0. [release] (#105)

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2.7/Dockerfile -t cimg/ruby:2.7.7 -t cimg/ruby:2.7 .
-docker build --file 2.7/node/Dockerfile -t cimg/ruby:2.7.7-node -t cimg/ruby:2.7-node .
-docker build --file 2.7/browsers/Dockerfile -t cimg/ruby:2.7.7-browsers -t cimg/ruby:2.7-browsers .
-docker build --file 3.0/Dockerfile -t cimg/ruby:3.0.5 -t cimg/ruby:3.0 .
-docker build --file 3.0/node/Dockerfile -t cimg/ruby:3.0.5-node -t cimg/ruby:3.0-node .
-docker build --file 3.0/browsers/Dockerfile -t cimg/ruby:3.0.5-browsers -t cimg/ruby:3.0-browsers .
+docker build --file 3.2/Dockerfile -t cimg/ruby:3.2.0 -t cimg/ruby:3.2 .
+docker build --file 3.2/node/Dockerfile -t cimg/ruby:3.2.0-node -t cimg/ruby:3.2-node .
+docker build --file 3.2/browsers/Dockerfile -t cimg/ruby:3.2.0-browsers -t cimg/ruby:3.2-browsers .
