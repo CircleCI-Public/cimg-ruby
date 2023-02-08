a2d35a10f5bd5aa66667f9366ef9893e5deac08b
Jeff Chen <jeff.chen@circleci.com>
2023-02-08 12:20:47 -0800

    Publish v3.2.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.2/Dockerfile -t cimg/ruby:3.2.0 -t cimg/ruby:3.2 .
-docker build --file 3.2/node/Dockerfile -t cimg/ruby:3.2.0-node -t cimg/ruby:3.2-node .
-docker build --file 3.2/browsers/Dockerfile -t cimg/ruby:3.2.0-browsers -t cimg/ruby:3.2-browsers .
+docker build --file 3.2/Dockerfile -t cimg/ruby:3.2.1 -t cimg/ruby:3.2 .
+docker build --file 3.2/node/Dockerfile -t cimg/ruby:3.2.1-node -t cimg/ruby:3.2-node .
+docker build --file 3.2/browsers/Dockerfile -t cimg/ruby:3.2.1-browsers -t cimg/ruby:3.2-browsers .
