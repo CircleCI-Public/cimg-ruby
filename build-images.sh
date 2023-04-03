fc0295e8c1e5fddb27971ea4c8ff34b99e18b005
Kevin J. Qiu <kevinjqiu@users.noreply.github.com>
2023-04-03 16:09:15 -0400

    Update 2.7 branch to 2.7.8 to fix CVE-2023-28756 (#114)
    * Update 2.7 branch to 2.7.8 to fix [CVE-2023-28756](https://www.ruby-lang.org/en/news/2023/03/30/redos-in-time-cve-2023-28756/)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.2/Dockerfile -t cimg/ruby:3.2.2 -t cimg/ruby:3.2 .
-docker build --file 3.2/node/Dockerfile -t cimg/ruby:3.2.2-node -t cimg/ruby:3.2-node .
-docker build --file 3.2/browsers/Dockerfile -t cimg/ruby:3.2.2-browsers -t cimg/ruby:3.2-browsers .
+docker build --file 2.7/Dockerfile -t cimg/ruby:2.7.8 -t cimg/ruby:2.7 .
+docker build --file 2.7/node/Dockerfile -t cimg/ruby:2.7.8-node -t cimg/ruby:2.7-node .
+docker build --file 2.7/browsers/Dockerfile -t cimg/ruby:2.7.8-browsers -t cimg/ruby:2.7-browsers .
