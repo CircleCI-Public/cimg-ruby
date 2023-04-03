7f57f8f078335306392d76e473828393b545e21f
Stuart Chinery <163900+schinery@users.noreply.github.com>
2023-04-03 21:39:48 +0100

    Publish v3.1.4. [release] (#123)

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.0/Dockerfile -t cimg/ruby:3.0.6 -t cimg/ruby:3.0 .
-docker build --file 3.0/node/Dockerfile -t cimg/ruby:3.0.6-node -t cimg/ruby:3.0-node .
-docker build --file 3.0/browsers/Dockerfile -t cimg/ruby:3.0.6-browsers -t cimg/ruby:3.0-browsers .
+
+docker build --file 3.1/Dockerfile -t cimg/ruby:3.1.4 -t cimg/ruby:3.1 .
+docker build --file 3.1/node/Dockerfile -t cimg/ruby:3.1.4-node -t cimg/ruby:3.1-node .
+docker build --file 3.1/browsers/Dockerfile -t cimg/ruby:3.1.4-browsers -t cimg/ruby:3.1-browsers .
 
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/ruby:3.0
-docker push cimg/ruby:3.0.6
-docker push cimg/ruby:3.0-node
-docker push cimg/ruby:3.0.6-node
-docker push cimg/ruby:3.0-browsers
-docker push cimg/ruby:3.0.6-browsers
+
+docker push cimg/ruby:3.1
+docker push cimg/ruby:3.1.4
+docker push cimg/ruby:3.1-node
+docker push cimg/ruby:3.1.4-node
+docker push cimg/ruby:3.1-browsers
+docker push cimg/ruby:3.1.4-browsers
+
