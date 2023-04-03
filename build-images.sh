5ffe0ec36a35d6fed794a5983b53a236b7aa8fa1
akinomaeni <akinomaeni@gmail.com>
2023-04-04 05:24:10 +0900

    Publish v3.0.6. [release] (#116)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2.7/Dockerfile -t cimg/ruby:2.7.8 -t cimg/ruby:2.7 .
-docker build --file 2.7/node/Dockerfile -t cimg/ruby:2.7.8-node -t cimg/ruby:2.7-node .
-docker build --file 2.7/browsers/Dockerfile -t cimg/ruby:2.7.8-browsers -t cimg/ruby:2.7-browsers .
+docker build --file 3.0/Dockerfile -t cimg/ruby:3.0.6 -t cimg/ruby:3.0 .
+docker build --file 3.0/node/Dockerfile -t cimg/ruby:3.0.6-node -t cimg/ruby:3.0-node .
+docker build --file 3.0/browsers/Dockerfile -t cimg/ruby:3.0.6-browsers -t cimg/ruby:3.0-browsers .
+
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/ruby:2.7
-docker push cimg/ruby:2.7.8
-docker push cimg/ruby:2.7-node
-docker push cimg/ruby:2.7.8-node
-docker push cimg/ruby:2.7-browsers
-docker push cimg/ruby:2.7.8-browsers
+docker push cimg/ruby:3.0
+docker push cimg/ruby:3.0.6
+docker push cimg/ruby:3.0-node
+docker push cimg/ruby:3.0.6-node
+docker push cimg/ruby:3.0-browsers
+docker push cimg/ruby:3.0.6-browsers
