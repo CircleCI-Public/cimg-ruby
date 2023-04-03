ba81bec854b76a7d2206975c4821b804954aa3b9
Adam Hartley <git@ahartley.com>
2023-04-04 00:12:09 +0100

    Publish v2.7.8. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-
-docker build --file 3.1/Dockerfile -t cimg/ruby:3.1.4 -t cimg/ruby:3.1 .
-docker build --file 3.1/node/Dockerfile -t cimg/ruby:3.1.4-node -t cimg/ruby:3.1-node .
-docker build --file 3.1/browsers/Dockerfile -t cimg/ruby:3.1.4-browsers -t cimg/ruby:3.1-browsers .
-
+docker build --file 2.7/Dockerfile -t cimg/ruby:2.7.8 -t cimg/ruby:2.7 .
+docker build --file 2.7/node/Dockerfile -t cimg/ruby:2.7.8-node -t cimg/ruby:2.7-node .
+docker build --file 2.7/browsers/Dockerfile -t cimg/ruby:2.7.8-browsers -t cimg/ruby:2.7-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/ruby:3.1
-docker push cimg/ruby:3.1.4
-docker push cimg/ruby:3.1-node
-docker push cimg/ruby:3.1.4-node
-docker push cimg/ruby:3.1-browsers
-docker push cimg/ruby:3.1.4-browsers
-
+docker push cimg/ruby:2.7
+docker push cimg/ruby:2.7.8
+docker push cimg/ruby:2.7-node
+docker push cimg/ruby:2.7.8-node
+docker push cimg/ruby:2.7-browsers
+docker push cimg/ruby:2.7.8-browsers
