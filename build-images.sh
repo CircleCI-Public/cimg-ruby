147094574476e56d570d6d20742535bce8ce913e
Adam Hartley <git@ahartley.com>
2023-07-24 22:42:21 +0100

    Test build on arm 2xlarge

--- build-images.sh
+++ build-images.sh
@@ -2,6 +2,7 @@
 # Do not edit by hand; please use build scripts/templates to make changes
 set -eo pipefail
 
+docker run -e QEMU_CREDENTIAL=yes -e QEMU_PERSISTENT=yes --privileged aptman/qus:latest -s -- -p
 docker context create cimg
 docker buildx create --use cimg
 docker buildx build --platform=linux/amd64,linux/arm64 --file 3.2/Dockerfile -t cimg/ruby:3.2.2 -t cimg/ruby:3.2 --push .
