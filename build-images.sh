9531f793163262acc82ddf4df7c3e78feba42071
ryanbourdais <ryanbourdais@outlook.com>
2023-11-15 09:11:52 -0600

    Publish view3. [release]

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,3 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.0/Dockerfile -t cimg/ruby:3.0.6 -t cimg/ruby:3.0 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.0/node/Dockerfile -t cimg/ruby:3.0.6-node -t cimg/ruby:3.0-node --push .
-docker buildx build --platform=linux/amd64 --file 3.0/browsers/Dockerfile -t cimg/ruby:3.0.6-browsers -t cimg/ruby:3.0-browsers --push .
