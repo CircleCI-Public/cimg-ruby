6020abca44ec4a7731bb314b039407a1ea95b4f5
Anton Halim <github@antonhalim.com>
2024-04-23 18:31:18 -0400

    Create and publish Ruby@3.3.1

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.2/Dockerfile -t cimg/ruby:3.2.3 -t cimg/ruby:3.2 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.2/node/Dockerfile -t cimg/ruby:3.2.3-node -t cimg/ruby:3.2-node --push .
-docker buildx build --platform=linux/amd64 --file 3.2/browsers/Dockerfile -t cimg/ruby:3.2.3-browsers -t cimg/ruby:3.2-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 3.3/Dockerfile -t cimg/ruby:3.3.1 -t cimg/ruby:3.2 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 3.3/node/Dockerfile -t cimg/ruby:3.3.1-node -t cimg/ruby:3.3.1-node --push .
+docker buildx build --platform=linux/amd64 --file 3.3/browsers/Dockerfile -t cimg/ruby:3.3.1-browsers -t cimg/ruby:3.3.1-browsers --push .
