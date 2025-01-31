9c011184b5ae2b36a055b573c880f1eda7c34d24
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-01-31 14:36:02 -0500

    Remove the already done

--- build-images.sh
+++ build-images.sh
@@ -8,9 +8,12 @@ docker buildx create --use ccitest
 # docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/2.7.7-browsers/Dockerfile -t ccitest/ruby:2.7.7-browsers --push .
 # docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/2.7.8-browsers/Dockerfile -t ccitest/ruby:2.7.8-browsers --push .
 # docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/3.0.5-browsers/Dockerfile -t ccitest/ruby:3.0.5-browsers --push .
+# docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/3.1.3-browsers/Dockerfile -t ccitest/ruby:3.1.3-browsers --push .
+
 # docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/3.0.6-browsers/Dockerfile -t ccitest/ruby:3.0.6-browsers --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/3.1.3-browsers/Dockerfile -t ccitest/ruby:3.1.3-browsers --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/3.1.4-browsers/Dockerfile -t ccitest/ruby:3.1.4-browsers --push .
+
+# docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/3.1.4-browsers/Dockerfile -t ccitest/ruby:3.1.4-browsers --push .
+
 docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/3.2.0-browsers/Dockerfile -t ccitest/ruby:3.2.0-browsers --push .
 docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/3.2.1-browsers/Dockerfile -t ccitest/ruby:3.2.1-browsers --push .
 docker buildx build --platform=linux/amd64,linux/arm64 --file temp-browser-fixes/3.2.2-browsers/Dockerfile -t ccitest/ruby:3.2.2-browsers --push .
